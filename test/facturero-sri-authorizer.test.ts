66e1ab24e6aaa985e81a3f16f554119e7055245f
Wladimir Trujillo <wladimir.trujillo@outlook.com>
2026-02-16 19:22:21 -0500

    init: project cdk

---
+++ test/facturero-sri-authorizer.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FactureroSriAuthorizer from '../lib/facturero-sri-authorizer-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/facturero-sri-authorizer-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FactureroSriAuthorizer.FactureroSriAuthorizerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
